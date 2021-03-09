c6f34769e67fe889f34ccacbf1fc85074e4f6601
itnams <68367990+itnams@users.noreply.github.com>
2021-03-09 20:00:33 +0700

    Initial Commit

---
+++ ContactsApp/ContactsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsAppApp.swift
+//  ContactsApp
+//
+//  Created by namnguyen on 09/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
